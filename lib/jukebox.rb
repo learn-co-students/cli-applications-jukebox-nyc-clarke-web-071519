a0954a9c2e425a789badecc9d46b4a62edca4ae9
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-13 16:41:41 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,5 @@
 # Add your code here
-songs = [
+SONGS = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
   "Sufjan Stevens - Too Much",
@@ -18,3 +18,66 @@ def help
 - play : lets you choose a song to play
 - exit : exits this program"
 end
+
+class String
+    def is_i?
+       !!(self =~ /\A[-+]?[0-9]+\z/)
+    end
+end
+
+def play(songs)
+  song = gets.chomp
+  if song.is_i? && song.to_i <= songs.length
+    son = songs[song.to_i-1]
+    puts "Playing #{son}"
+    return
+  else
+    songs.each do |son|
+      if son == song
+        puts "Playing #{son}"
+        return
+      end
+    end
+  end
+  puts "/Invalid input, please try again/"
+end
+
+def list(songs)
+  num = 0
+  songs.each do |song|
+    num += 1
+    puts "#{num}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts "Please enter a command:"
+  answer = gets.chomp
+  if answer == 'play'
+    puts "Please enter a song name or number:"
+    play(songs)
+  elsif answer == 'help'
+    help
+    exit_jukebox
+  elsif answer == 'list'
+    list(songs)
+  elsif answer == 'exit'
+    exit_jukebox
+  end
+end
+
+
+# until answer == 'exit'
+#   if answer == 'play'
+#     play(songs)
+#   elsif answer == 'help'
+#     help
+#   elsif answer == 'list'
+#     list(songs)
+#   end
+# end
+# exit_jukebox
